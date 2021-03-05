d033f227924d99a8bbc593762c33d2478820bf6b
Borja LoFe <im@borjalofe.com>
2021-03-05 23:37:52 +0100

    Scully added as the project's SSG

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+
+const routes:Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ScullyLibModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
